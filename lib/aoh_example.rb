775c11514c0138a89bd7050cf689b9562ea76187
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-14 15:06:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -33,7 +33,7 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  
+  aoh[1][:name]<<"(PGOAT)"
   
 end
 
